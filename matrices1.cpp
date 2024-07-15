0c8ea3c35d9d4ebd8763f1e9eb1974a117268556
asocualayaf <asocualayaf@unjbg.edu.pe>
2024-07-15 10:08:40 -0500

    Subiendo el ejercicio 1 de arreglos bidimensionales

---
+++ matrices1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n, s = 0;
+	int A[20][20];
+	cout<<"Ingrese el orden de la matriz cuadrada: "<<endl; cin>>n;
+	for(int i=0; i<n; i++){
+		for(int j = 0; j<n; j++){
+			cout<<"Ingrese el valor del elemento A["<<i+1<<" , "<<j+1<<" ]"<<endl; cin>>A[i][j];
+			if (i < j ){
+				s = s + A[i][j];
+			}
+		}
+	}
+	cout<<"Sumatoria de elementos por encima de la diagonal principal = "<<s<<endl;
+	return 0;
+	
+}
